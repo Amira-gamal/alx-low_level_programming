7b825c7e965fd04bfe4f82e3bcfcc2c043695402
Amira-gamal <amiragamal7814@gmail.com>
2023-08-11 16:46:45 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lowercase followed by a new line
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = ‘a’; x <= ‘z’; x++)
+		putchar(x);
+	putchar (‘\n’);
+	return (0);
+}
