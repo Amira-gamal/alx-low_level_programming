483c5163625004b6604ab32fdd8fe43a72635583
Amira-gamal <amiragamal7814@gmail.com>
2023-08-11 17:25:09 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lowercase followed by a new line
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != e && x != q)
+			putchar(x);
+	}
+	putchar ('\n');
+	return (0);
+}
