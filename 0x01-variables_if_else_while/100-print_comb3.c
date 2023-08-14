a0820498c5d05a593eb29cfa4871db9969fa5d70
Amira-gamal <amiragamal7814@gmail.com>
2023-08-14 16:29:41 -0700

    mmmn

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * A program that prints all possible different combinations of two digits
+ * Return: 0 if success.
+*/
+int main(void)
+{
+	int i, x;
+
+	for (i = '0'; i <= '9', i++)
+	{
+		for (x = '0'; x <= '9', x++)
+		{
+			if (i != x && i < x)
+			{
+				putchar(i);
+				putchar(x);
+				if (i == '8' && x == '9')
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
