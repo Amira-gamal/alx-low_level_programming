5f17b98ccd36fc4ce722f7b9772fc3ac2de9c645
Amira-gamal <amiragamal7814@gmail.com>
2023-08-15 16:22:17 -0700

    mmn

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,13 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+	int x[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+		_putchar('\n');
+	}
 	return (0);
 }
