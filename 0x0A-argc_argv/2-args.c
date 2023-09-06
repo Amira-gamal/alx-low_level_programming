c52d6d5c6900472ede1e10927a0c9feb3a0508e9
Amira-gamal <amiragamal7814@gmail.com>
2023-09-06 11:16:29 -0700

    kkk

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
-* main - Prints all arguments it receives followed by a new line
+* main-Prints all arguments it receives.
 * @argc: number of arguments
 * @argv: array of arguments
 * Return: Always 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i;
 
