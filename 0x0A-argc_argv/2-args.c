673a5799a3d5cc0465d7aba63cf608a29eff9dce
Amira-gamal <amiragamal7814@gmail.com>
2023-09-06 15:34:23 -0700

    kkkkk

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
-* main-Prints all arguments it receives.
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: Always 0
+/**
+ * main - A program that prints all arguements it receives
+ * @argc: The arguement counter
+ * @argv: The arguement values
+ * Return: Always 0 (Success)
 */
 
 int main(int argc, char **argv)
