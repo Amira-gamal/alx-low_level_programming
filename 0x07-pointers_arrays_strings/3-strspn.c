2f9e2194e418aff876e02985a1002fa371cbc4e7
Amira-gamal <amiragamal7814@gmail.com>
2023-09-06 06:21:07 -0700

    ggggg

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strspn - Gets the length of a prefix substring.
+* @s: The string to be searched.
+* @accept: The prefix to be measured.
+* Return: The number of bytes in s which
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[x] == accept[i])
+				break;
+		}
+		if (s[x] == '\0')
+			break;
+	}
+	return (x);
+}
