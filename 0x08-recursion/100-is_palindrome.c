aa4d411bf78d0e01e2b4f1d38cbc4c8e8008bfe1
Amira-gamal <amiragamal7814@gmail.com>
2023-10-26 05:08:42 -0700

    kkkkk

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,52 +1,51 @@
 #include "main.h"
-int strlen_recursion(char *s);
-int pali_checker(char *s, int l);
+
+
+/**
+ * _strlen_recursion - find length of a string
+ * @s: pointer to string to be counted
+ * Return: return count
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * charcheck - resolve palidrome
+ * @str: pointer to string to be counted
+ * @length: int type
+ * Return: return palidrome
+ */
+
+int charcheck(char *str, int length)
+{
+	if (length <= 0)
+		return (1);
+	if (str[0] == str[length - 1])
+	{
+		return (charcheck(str + 1, length - 2));
+	}
+	else
+		return (0);
+}
 
 /**
  * is_palindrome - check if palidrome
  * @s: pointer to string to be counted
  * Return: return value
-*/
+ */
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	int length = _strlen_recursion(s);
 
-	if (len <= 1)
+	if (length <= 1)
 		return (1);
 	else
-		return (pali_checker(s, len));
-}
-
-/**
- * strlen_recursion - find length of a string
- * @s: string to be counted
- * Return: return count
-*/
-
-int strlen_recursion(char *s)
-{
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
-	else
-		return (0);
-}
-
-/**
- * pali_checker - resolve palidrome
- * @s: pointer to string to be counted
- * @l: int type
- * Return: return palidrome
- */
-
-int pali_checker(char *s, int l)
-{
-	if (l <= 0)
-		return (1);
-	if (s[0] == s[l - 1])
-	{
-		return (pali_checker(s + 1, l - 2));
-	}
-	else
-		return (0);
+		return (charcheck(s, length));
 }
