fd87eccafe5b629eb68a22aaccf2fd37728c507a
Amira-gamal <amiragamal7814@gmail.com>
2023-10-27 16:19:02 -0700

    llll

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,23 +7,22 @@
  * @argv: input
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int i, x, y = 0;
+	int i, j, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (x = 0; argv[i][x] != '\0'; x++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][x]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		y += atoi(argv[i]);
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", y);
+	printf("%d\n", add);
 	return (0);
 }
