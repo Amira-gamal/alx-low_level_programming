7e56548d1936c839d5920360e5fa85a7ddb4194d
Amira-gamal <amiragamal7814@gmail.com>
2023-10-31 17:52:02 -0700

    jjjj

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Main Entry
+ * @str: input
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *ds;
+	int i = 0, x;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	ds = (char *)malloc((i + 1) * sizeof(char));
+	if (ds == NULL)
+		return (NULL);
+	for (x = 0; x <= i; x++)
+		ds[x] = str[x];
+	return (ds);
+}
