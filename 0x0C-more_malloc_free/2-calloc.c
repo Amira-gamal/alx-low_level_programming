5547aef83607bf7c3cd09ac7959b42c8a944f82c
Amira-gamal <amiragamal7814@gmail.com>
2023-11-02 19:24:39 -0700

    kkkkk

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,14 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	void *ptr = malloc(nmemb * size);
+	char *ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	unsigned char *byte_ptr = (unsigned char *)ptr;
-
 	for (i = 0; i < (nmemb * size); i++)
-		byte_ptr[i] = 0;
+		ptr[i] = 0;
+
 	return (ptr);
 }
