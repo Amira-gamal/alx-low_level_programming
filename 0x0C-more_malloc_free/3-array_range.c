da7ea0fbcf3fa0e8cdae9aadb0b8df9cf7570272
Amira-gamal <amiragamal7814@gmail.com>
2023-11-02 19:51:08 -0700

    llll

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	arr = malloc(sizeof(*arr) * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 		arr[i] = min;
 	return (arr);
 }
