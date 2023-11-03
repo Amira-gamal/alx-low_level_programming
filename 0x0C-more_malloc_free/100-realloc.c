784f1952eb3b14a292d3acac232cb70e7ddfdc9f
Amira-gamal <amiragamal7814@gmail.com>
2023-11-03 06:43:09 -0700

    gghhh

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - ...
+ * @ptr: ...
+ * @old_size: ...
+ * @new_size: ...
+ *
+ * Return: ...
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, min_size;
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return(NULL);
+	min_size = (old_size < new_size) ? old_size : new_size;
+	for (i = 0; i < min_size; i++)
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (new_ptr);
+}
