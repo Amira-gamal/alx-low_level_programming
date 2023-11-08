9e04cb1d6bb0663803421df0eb0752fe9ab42f41
Amira-gamal <amiragamal7814@gmail.com>
2023-11-07 23:15:03 -0800

    lllll

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
+*/
+
+int main(int argc, char *argv[])
+{
+	int num_bytes, i = 0;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	for  (i = 0; i < num_bytes; i++)
+	{
+		if (i == num_bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
