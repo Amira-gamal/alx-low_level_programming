1ec1f7126d07443c82229883b85966b0fc93f278
Amira-gamal <amiragamal7814@gmail.com>
2023-11-09 15:33:38 -0800

    lllll

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - ...
+ * @n: ...
+ *
+ * Return: ...
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
