69155589c9075b4babe2f0ee67cb68223ad937b7
Amira-gamal <amiragamal7814@gmail.com>
2023-11-17 16:51:05 -0800

    sdfg

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	size_t n;
 
-	if (str != NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	if (new_node == NULL)
-		return (NULL);
-	new_node->next = *head;
 	new_node->str = strdup(str);
-	if (new_node->str)
-	{
-		for (i = 0; new_node->str[i]; ++i)
-			;
-	}
-	new_node->len = i;
+	for (n = 0; str[n]; n++)
+		;
+
+	new_node->len = n;
+	new_node->next = *head;
 	*head = new_node;
-	return (new_node);
+	return (*head);
 }
