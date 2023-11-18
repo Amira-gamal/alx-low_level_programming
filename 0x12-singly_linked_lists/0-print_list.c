cdd0c85b30a616a5b1cc2ff4e7de1640a6f94604
Amira-gamal <amiragamal7814@gmail.com>
2023-11-17 16:07:59 -0800

    kjhgfd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,12 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++)
+	for (i = 0; h != NULL; h = h->next; i++)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
 	}
 	return (i);
 }
