cb0c778c9092bc983202e2088176f7b5eeba075d
Amira-gamal <amiragamal7814@gmail.com>
2023-11-23 17:04:16 -0800

    sdfgh

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,13 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	 unsigned long int xor_result = n ^ m;
-	 unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
 
-	 while (xor_result)
-	 {
-		 count += (xor_result & 1);
-		 xor_result >>= 1;
-	 }
-	 return (count);
+	while (xor_result)
+	{
+		count += (xor_result & 1);
+		xor_result >>= 1;
+	}
+	return (count);
 }
