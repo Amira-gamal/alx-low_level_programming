fed534d7c330eec39b8599376274bd230d2aca76
Amira-gamal <amiragamal7814@gmail.com>
2023-11-23 16:51:57 -0800

    dfgh

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * clear_bit - sets a bit
+ * @n: number
+ * @index: position
+ * Return: 1 on success, -1 on fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int size = (sizeof(unsigned long int) * 8) - 1;
+
+	if (index >= size)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
