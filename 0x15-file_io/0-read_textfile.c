aa41467330f52493f37f512e2ca119af771b740f
Amira-gamal <amiragamal7814@gmail.com>
2023-11-27 11:11:46 -0800

    asdfg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,32 +11,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor = open(filename, O_RDONLY);
-	char *buffer = (char *)malloc(sizeof(char) * letters);
-	ssize_t bytes_read = read(file_descriptor, buffer, letters);
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
+	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
-	if (buffer == NULL)
-	{
-		close(fil0e_descriptor);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
-	}
-	if (bytes_read == -1)
-	{
-		close(file_descriptor);
-		free(buffer);
-		return (0);
-	}
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(file_descriptor);
-		free(buffer);
-		return (0);
-	}
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	close(file_descriptor);
 	free(buffer);
 	return (bytes_written);
