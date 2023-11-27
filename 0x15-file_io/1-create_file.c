5e747319b062b4eef2db8767757d66d94edecc42
Amira-gamal <amiragamal7814@gmail.com>
2023-11-27 11:37:11 -0800

    dfghj

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, bytes_written, i = 0;
+	int op, bytes_written, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i];)
-			i++;
+		for (i = 0; text_content[i]; i++)
+			;
 	}
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	bytes_written = write(o, text_content, len);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_written = write(op, text_content, i);
 
-	if (file_descriptor == -1 || bytes_written == -1)
+	if (op == -1 || bytes_written == -1)
 		return (-1);
 
-	close(file_descriptor);
+	close(op);
 
 	return (1);
 }
